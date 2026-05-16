4bb24898d625cc9628e9201795de766879b9fd8a
rohaoh <rohaoh@gmail.com>
2026-05-16 09:50:04 +0900

    Add files via upload

---
+++ 2026-05-16/d8-1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void){
+	char a[10];
+	int b[9];
+	
+	
+	// a="inform"; (안되는 예시)
+	a[0]='i';
+	a[1]='n';
+	a[2]='f';
+	a[3]='o';
+	a[4]='r';
+	a[5]='m';
+	//맞는 예시 
+	b[0]=10;
+	b[1]=20;
+	
+	
+	return 0;
+}
---
+++ 2026-05-16/d8-2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+	char a[10]="apple";
+	
+	printf("%c", a[0]);
+	
+	return 0;
+}
